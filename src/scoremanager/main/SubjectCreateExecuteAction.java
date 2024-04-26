b2e9e6f6c7b1a17768fc492dab8093f801d8115a
matsushita <tky2302098@stu.o-hara.ac.jp>
2024-04-26 12:40:56 +0900

    aaa

--- src/scoremanager/main/SubjectCreateExecuteAction.java
+++ src/scoremanager/main/SubjectCreateExecuteAction.java
@@ -1,12 +1,13 @@
 package scoremanager.main;
 
-import javax.security.auth.Subject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import bean.School;
+import bean.Subject;
 import bean.Teacher;
+import dao.SubjectDao;
 import tool.Action;
 
 public class SubjectCreateExecuteAction extends Action {
