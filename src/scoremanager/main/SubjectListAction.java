bf79af434f2f38b58d64aa4ebe00b71886f82e57
matsushita <tky2302098@stu.o-hara.ac.jp>
2024-04-26 12:24:47 +0900

    科目登録の新規作成

---
+++ src/scoremanager/main/SubjectListAction.java
@@ -0,0 +1,47 @@
+package scoremanager.main;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Student;
+import bean.Teacher;
+import dao.StudentDao;
+import tool.Action;
+
+public class SubjectListAction extends Action {
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+	    HttpSession session = req.getSession();
+	    Teacher teacher = (Teacher)session.getAttribute("user");
+
+	    //変数宣言タイム
+	    String entYearStr="";
+	    String classNum="";
+	    String isAttendStr="";
+	    int entYear = 0;
+	    boolean isAttend = false;
+	    List<Student> students = null;
+	    LocalDate todaysDate = LocalDate.now();
+	    int year = todaysDate.getYear();
+	    StudentDao sDao = new StudentDao();
+	    SubjectDao subjectDao = new SubjectDao();
+	    Map<String, String> errors = new HashMap<>();
+
+	  //ログインユーザーの学校コードをもとにクラス番号一覧の取得
+	    List<String> list = subjectDao.filter(teacher.getSchool());
+
+
+	    req.setAttribute("subject_set", list);
+
+
+
+	    req.getRequestDispatcher("subject_list.jsp").forward(req,  res);
+	}
+}
